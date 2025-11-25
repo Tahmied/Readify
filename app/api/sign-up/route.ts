eb2f909e1700cce01bf5236e1b90b17e0d29993e
Tahmied Hossain <tahmiedhossain4671@gmail.com>
2025-11-25 21:00:27 +0600

    - added user data model and a register user api route

--- app/api/sign-up/route.ts
+++ app/api/sign-up/route.ts
@@ -1,9 +1,33 @@
 import dbConnect from "@/lib/dbConnect";
-import { NextResponse } from "next/server";
+import User from "@/model/User";
+import bcrypt from "bcryptjs";
 
-export async function GET(request: Request) {
-    await dbConnect()
-    return NextResponse.json({
-        'message': "first api in nextjs"
-    })
+export async function POST(req: Request) {
+    try {
+        await dbConnect()
+
+        const { name, email, password } = await req.json()
+
+        const existingUser = await User.findOne({ email })
+        if (existingUser) {
+            return new Response(JSON.stringify({ error: "User already exists" }), {
+                status: 400,
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+        })
+
+        return new Response(JSON.stringify({ message: "User created", user }), {
+            status: 201,
+        })
+    } catch (error) {
+        return new Response(JSON.stringify({ error }), { status: 500 })
+    }
 }
+
