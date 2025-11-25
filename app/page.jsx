7208b15612bbaa0dcbfee752bafeed78cd5d29be
Tahmied Hossain <tahmiedhossain4671@gmail.com>
2025-11-25 10:04:11 +0600

    - added hero section

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 
 import Header from '../components/Header';
+import Hero from '../components/Homepage/Hero';
 
 export default function Home() {
   return (
     <div>
       <Header></Header>
+      <Hero></Hero>
     </div>
   );
 }
---
+++ components/Homepage/Hero.jsx
@@ -0,0 +1,19 @@
+
+const Hero = () => {
+    return (
+        <section className='hero bg-white'>
+            <div className="hero-container flex items-center justify-between max-w-[1300px] mx-auto h-[70vh] mt-[3rem]">
+                <div className="hero-left flex items-start justify-center gap-4 flex-col w-[80%] max-w-[600px]">
+                    <p className="hero-title text-4xl text-black">Readify - Here Every Book Tells a Story</p>
+                    <p className="hero-desc text-xl text-black">At Readify, we believe that books are more than just pages; they are a doorway to new worlds, ideas and adventures. </p>
+                    <button className="cta-btn border-[1px] border-[#eb7c1f] text-[#eb7c1f] bg-[#f8ecdc] font-semibold px-[35px] py-[15px] rounded-[15px] cursor-pointer transition-all duration-300 hover:translate-y-[-5px] hover:shadow-xl">
+                        Shop Now
+                    </button>
+                </div>
+                <img src="/hero/hero-img.png" alt="" className="hero-img max-w-[500px]" />
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
