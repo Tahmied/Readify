2dd6dbc9a61de5f56036fa338abac7ca33b00823
Tahmied Hossain <tahmiedhossain4671@gmail.com>
2025-11-27 10:57:54 +0600

    - fixed vercel deployment issue and added dynamic title for the login page

--- app/(books)/edit-book/[id]/page.jsx
+++ app/(books)/edit-book/[id]/page.jsx
@@ -3,6 +3,27 @@ import Book from "@/model/Book";
 import { ArrowLeft, BookOpen } from 'lucide-react';
 import Form from "./Form";
 
+
+export async function generateMetadata({ params }) {
+  const { id } = await params;
+
+  await dbConnect();
+  const book = await Book.findById(id).lean();
+
+  if (!book) {
+    return {
+      title: "Book Not Found",
+      description: "The requested book could not be found."
+    };
+  }
+
+  return {
+    title: `${book.title}`,
+    description: book.description.substring(0, 100)
+  };
+}
+
+
 const EditBook = async ({ params }) => {
   const { id } = await params
   await dbConnect()
